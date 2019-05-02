e56e43fc3684fe592728890d48c9df5b43232409
Danee de Kruyff <daneedekruyff@MBP-van-Danee.dynamic.ziggo.nl>
2019-05-02 20:17:56 +0200

    Added battery low indicator, started infopanel

--- app/js/homey.ink.app.js
+++ app/js/homey.ink.app.js
@@ -5,22 +5,64 @@ window.addEventListener('load', function() {
   
   var homey;
   var me;
-  var Sunrise = "";
-  var Sunset = "";
+  var sunrise = "";
+  var sunset = "";
+  var batteryWarning =[];
   
   var $header = document.getElementById('header');
+  var $text = document.getElementById('text');
   var $textLarge = document.getElementById('text-large');
   var $textSmall = document.getElementById('text-small');
   var $logo = document.getElementById('logo');
+  var $weather = document.getElementById('weather');
   var $weatherTemperature = document.getElementById('weather-temperature');
   var $weatherState = document.getElementById('weather-state');
   var $flowsInner = document.getElementById('flows-inner');
   var $devicesInner = document.getElementById('devices-inner');
   
+  var $infopanel = document.createElement('div');
+  $infopanel.id = "info-panel";
+  $infopanel.classList.add('info-panel');
+  $header.appendChild($infopanel);
+
+  var $sunevents = document.createElement('div');
+  $sunevents.id = 'sun-events';
+  $header.appendChild($sunevents)
+
+  var $batterywarning = document.createElement('div');
+  $batterywarning.id = 'battery-warning';
+  $header.appendChild($batterywarning);
+
+  $infopanel.addEventListener('click', function() {
+    $infopanel.style.visibility = "hidden";
+  });
+
   $logo.addEventListener('click', function(){
     window.location.reload();
   });
-  
+
+  $text.addEventListener('click', function() {
+    homey.notifications.getNotifications().then(function(notifications) {
+      return renderInfoPanel('t',notifications);
+    })
+  });
+
+  $weather.addEventListener('click', function() {
+    homey.weather.getWeather().then(function(weather) {
+      return renderInfoPanel("w", weather)
+    }).catch(console.error);
+  })
+
+  $sunevents.addEventListener('click', function() {
+    homey.weather.getWeather().then(function(weather) {
+      return renderInfoPanel("w", weather)
+    }).catch(console.error);
+  })
+
+  $batterywarning.addEventListener('click', function() {
+    return renderInfoPanel("b")
+  })
+
   renderText();
   later.setInterval(function(){
     renderText();
@@ -73,17 +115,32 @@ window.addEventListener('load', function() {
       }).catch(console.error);
 
       homey.flowToken.getFlowTokens().then(function(tokens) {
-        console.log(tokens)
         for (let token in tokens) {
           if ( tokens[token].id == "sunrise" ) {
-            Sunrise = tokens[token].value
+            sunrise = tokens[token].value
           }
           if ( tokens[token].id == "sunset"  ) {
-            Sunset = tokens[token].value
-          }          
+            sunset = tokens[token].value
+          }
+          if ( tokens[token].id == "alarm_battery" && tokens[token].value == true ) {
+            var batteryLevel
+            for (let ttoken in tokens) {
+              if (tokens[ttoken].uriObj.id == tokens[token].uriObj.id && tokens[ttoken].id == "measure_battery" ) {
+                batteryLevel = tokens[ttoken].value
+              }
+            }
+            var element = {}
+            element.name = tokens[token].uriObj.name
+            element.zone = tokens[token].uriObj.meta.zoneName
+            element.level = batteryLevel
+            batteryWarning.push(element)
+          }
         }
-        if (Sunrise != "" || Sunset != "") {
+        if (sunrise != "" || sunset != "") {
           renderSunevents();
+        }
+        if ( Object.keys(batteryWarning).length ) {
+          renderBatteryWarning(batteryWarning);
         }
       }).catch(console.error);
 
@@ -125,10 +182,52 @@ window.addEventListener('load', function() {
     }).catch(console.error);
   }
   
+  function renderInfoPanel(type,info) {
+    switch(type) {
+      case "t":
+        $infopanel.innerHTML = 'Notifications';
+
+        break;
+      case "w": 
+        $infopanel.innerHTML = '';
+        var $infopanelTemperature = document.createElement('div');
+        $infopanelTemperature.id = "weather-temperature"
+        $infopanel.appendChild($infopanelTemperature);
+        $infopanelTemperature.innerHTML = Math.round(info.temperature*10)/10;
+        
+        var $infopanelState = document.createElement('div');
+        $infopanelState.id = "weather-state"
+        $infopanel.appendChild($infopanelState);
+        $infopanelState.innerHTML = "";
+        $infopanelState.classList.add('weather-state');
+        var $icon = document.createElement('div');
+        $icon.classList.add('icon');
+        $icon.classList.add(info.state.toLowerCase());
+        $icon.style.backgroundImage = 'url(../img/weather/' + info.state.toLowerCase() + '.svg)';    
+        $icon.style.webkitMaskImage = 'url(../img/weather/' + info.state.toLowerCase() + '.svg)';
+        $infopanelState.appendChild($icon)
+    
+        var $infopanelHumidity =  document.createElement('div');
+        $infopanelHumidity.id = "weather-humidity"
+        $infopanel.appendChild($infopanelHumidity);
+        $infopanelHumidity.innerHTML = " Humidity " + info.humidity*100 + "%";
+    
+        var $infopanelPressure = document.createElement('div');
+        $infopanelPressure.id = "weather-pressure"
+        $infopanel.appendChild($infopanelPressure);
+        pressure = info.pressure + " "
+        $infopanelPressure.innerHTML = pressure.replace('.','') + " mbar";     
+        break;
+      case "b":
+        $infopanel.innerHTML = 'Batterystatus';
+
+        break;
+    }
+    console.log("style")
+    $infopanel.style.visibility = "visible";
+  }
+
   function renderSunevents() {
-    var $sunevents = document.createElement('div');
-    $sunevents.id = 'sunevents';
-    $header.appendChild($sunevents)
     
     var $sunriseIcon = document.createElement('div');
     $sunriseIcon.id = 'sunrise';
@@ -138,7 +237,7 @@ window.addEventListener('load', function() {
     var $sunriseTime = document.createElement('div');
     $sunriseTime.id = 'sunrise-time';
     $sunevents.appendChild($sunriseTime);
-    $sunriseTime.innerHTML = Sunrise;
+    $sunriseTime.innerHTML = sunrise;
 
     var $sunsetIcon = document.createElement('div');
     $sunsetIcon.id = 'sunset';
@@ -148,7 +247,12 @@ window.addEventListener('load', function() {
     var $sunsetTime = document.createElement('div');
     $sunsetTime.id = 'sunset-time';
     $sunevents.appendChild($sunsetTime);   
-    $sunsetTime.innerHTML = Sunset;
+    $sunsetTime.innerHTML = sunset;
+  }
+
+  function renderBatteryWarning() {
+    $batterywarning.style.webkitMaskImage = 'url(../img/battery.png)';
+    $header.appendChild($batterywarning)
   }
 
   function renderWeather(weather) {
